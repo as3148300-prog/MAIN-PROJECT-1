210373552cf6c5c67a285f410f5783973b008990
Areeb Sheikh <as3148300@gmail.com>
2026-07-11 06:02:30 +0530

    My Project is Done!!!

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,42 +1,42 @@
-import React from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react'
+
+import { navLinks } from '../../constants/index.js'
 
 const Navbar = () => {
-  return (
-    <div  className='max-w-screen-2xl   px-7 mx-auto py-6   flex gap-10 '> 
-    <img  src="https://cdn.prod.website-files.com/62df9251ae9124d9d726bcb9/62dfc1419247bccdd5a2dd1b_Tools.svg" alt="" />
-    <div className='ml-15 flex gap-20'>
-         {
-        [
-        {
-            id:1,
-            name: "Home"
-        },
-        {
-            id:2,
-            name: "Work"
-        },
-         {
-            id:3,
-            name: "Culture"
-        },
-         {
-            id:4,
-            name: ""
-        }, {
-            id:5,
-            name: "News"
-        },
-        ].map((items,idx)=>{
-             return (
-               
-             <a key={items.id} href="">{items.name}
-             <span className='inline-block h-[10px] w-[10px]  bg-green-600'></span> </a>
-            )
-        })
-    }
-    </div>
-    </div>
-  )
+ useGSAP(() => {
+	const navTween = gsap.timeline({
+	 scrollTrigger: {
+		trigger: 'nav',
+		start: 'bottom top'
+	 }
+	});
+	
+	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+	 backgroundColor: '#00000050',
+	 backgroundFilter: 'blur(10px)',
+	 duration: 1,
+	 ease: 'power1.inOut'
+	});
+ })
+ 
+ return (
+	<nav>
+	 <div>
+		<a href="#home" className="flex items-center gap-2">
+		 <img src="/images/logo.png" alt="logo" />
+		 <p>Velvet Pour</p>
+		</a>
+		
+		<ul>
+		 {navLinks.map((link) => (
+			<li key={link.id}>
+			 <a href={`#${link.id}`}>{link.title}</a>
+			</li>
+		 ))}
+		</ul>
+	 </div>
+	</nav>
+ )
 }
-
 export default Navbar
